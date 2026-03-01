4f1f4015176415f4b667fde7aca12fc851b5fc0f
MrAlikhanov <alialixanov2007@gmail.com>
2026-03-01 21:32:13 +0400

    Update 3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -68,3 +68,13 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        """
+        Returns a string representation of the rectangle
+        using '#' characters.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        return "\n".join("#" * self.__width for _ in range(self.__height))
