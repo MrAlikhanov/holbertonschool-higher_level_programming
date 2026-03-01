c058e0df91f328e71596337f9f2c899b31ca16be
MrAlikhanov <alialixanov2007@gmail.com>
2026-03-01 22:37:16 +0400

    Create 5-base_geometry.py

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class BaseGeometry.
+This class will serve as the base for future geometry-related classes.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class representing base geometry.
+    """
+    pass
