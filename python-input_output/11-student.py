ad849c9e7a93a52a75c4e8e615063af136597c8b
MrAlikhanov <alialixanov2007@gmail.com>
2026-03-06 14:19:53 +0400

    Create 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module for Student class."""
+
+
+class Student:
+    """Defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
