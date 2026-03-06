7bf21a6fa0bb5a81580ce0cb66ec4ff162e2f3b0
MrAlikhanov <alialixanov2007@gmail.com>
2026-03-06 13:42:14 +0400

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module for reading a file and printing its content to stdout."""
+
+
 def read_file(filename=""):
+    """Read a text file (UTF8) and print it to stdout."""
     with open(filename, encoding="utf-8") as f:
-        print(f.read(), ends="")
+        print(f.read(), end="")
