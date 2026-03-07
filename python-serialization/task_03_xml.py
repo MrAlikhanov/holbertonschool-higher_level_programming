f116d5479e55b8699af7e1119999e2eed0b3c8cb
MrAlikhanov <alialixanov2007@gmail.com>
2026-03-07 18:21:16 +0400

    Update task_03_xml.py

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -3,3 +3,12 @@ import xml.etree.ElementTree as ET
 
 def serialize_to_xml(dictionary, filename):
     root = ET.Element("data")
+for key, value in dictionary.items():
+        child = ET.SubElement(root, key)
+        child.text = str(value)
+
+def deserialize_from_xml(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    return {child.tag: child.text for child in root}
