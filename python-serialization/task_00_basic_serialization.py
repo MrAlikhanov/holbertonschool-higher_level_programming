d769ad7e73ad88776565bc3b27344efba53d138f
MrAlikhanov <alialixanov2007@gmail.com>
2026-03-07 18:05:38 +0400

    Create task_00_basic_serialization.py

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import pickle
+
+# Serializing a dictionary
+data = {"name": "John", "age": 30, "city": "New York"}
+
+with open("data.pkl", "wb") as file:
+    pickle.dump(data, file)
+
+# Deserializing the data
+with open("data.pkl", "rb") as file:
+    loaded_data = pickle.load(file)
+
+print(loaded_data)
