b5bc54ee0075ac8d836a90bd5f5417b9b4aed62f
MrAlikhanov <alialixanov2007@gmail.com>
2026-03-07 18:24:46 +0400

    Create task_04_net.py

---
+++ python-serialization/task_04_net.py
@@ -0,0 +1,27 @@
+import socket
+import json
+
+HOST = '127.0.0.1'
+PORT = 65432
+
+
+def start_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind((HOST, PORT))
+        server_socket.listen(1)
+
+        conn, addr = server_socket.accept()
+        with conn:
+            chunks = []
+            while True:
+                data = conn.recv(4096)
+                if not data:
+                    break
+                chunks.append(data)
+
+            raw = b''.join(chunks)
+            dictionary = json.loads(raw.decode('utf-8'))
+
+            print("Received Dictionary from Client:")
+            print(dictionary)
