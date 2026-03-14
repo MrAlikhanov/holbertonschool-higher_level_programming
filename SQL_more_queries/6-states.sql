5be5ed514e86c6c039ff7ff1a631d6994a672f5c
MrAlikhanov <alialixanov2007@gmail.com>
2026-03-14 17:32:52 +0400

    Create 6-states.sql

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,17 @@
+-- Creates the database hbtn_0d_usa and the table states
+-- states description:
+--   id INT unique, auto generated, can't be null and is a primary key
+--   name VARCHAR(256) can't be null
+
+-- Create the database if it doesn't already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Switch to the created database
+USE hbtn_0d_usa;
+
+-- Create the table if it doesn't already exist
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
