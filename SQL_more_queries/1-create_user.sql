6af613607c1f5502462727a18a0c0bad2372871c
MrAlikhanov <alialixanov2007@gmail.com>
2026-03-14 17:20:40 +0400

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
--- 
+-- Creates the user user_0d_1 with all privileges and a specific password
+-- Does not fail if the user already exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
