88ef1a8e3daf4945dcd9f76aa4abefc09dee4f67
AdMrAlikhanov <alialixanov2007@gmail.com>
2026-03-15 18:49:13 +0000

    Add Task 12: Update state name (Arizona to New Mexico)

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Updates the name of State with id 2 to New Mexico"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, database),
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2).first()
+
+    state.name = "New Mexico"
+    session.commit()
+
+    session.close()
