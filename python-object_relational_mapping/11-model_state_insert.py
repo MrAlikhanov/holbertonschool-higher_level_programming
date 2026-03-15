ed7a44aeaf0b503c9bd786805908eeeb3c086747
AdMrAlikhanov <alialixanov2007@gmail.com>
2026-03-15 18:47:43 +0000

    Add Task 11: Insert new state (Louisiana) using SQLAlchemy

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Adds State Louisiana to hbtn_0e_6_usa database"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, database),
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
+
+    session.close()
